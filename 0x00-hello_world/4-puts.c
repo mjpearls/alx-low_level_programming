30c105772c340707b507735292f26e05c607a90d
mjpearls <makindehin2@gmail.com>
2022-10-16 09:20:48 -0700

    commit for program 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints 'Programming is like building a multilingual puzzle'
+ *
+ * Return: Always zero (success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
