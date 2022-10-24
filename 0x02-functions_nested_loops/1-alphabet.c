ad0ad8c1276dd07c3761ed56f7ba591f30dab6b1
mjpearls <makindehin2@gmail.com>
2022-10-23 17:07:34 -0700

    commit for 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - in small letters
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar (alphabet);
+		alphabet++;
+	}
+	_putchar ("\n");
+}
+
